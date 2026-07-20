0c9fc208d99ba013960e6abb44fd220b2f3245e3
sree <happyluckypotato@gmail.com>
2026-07-20 01:07:51 -0400

    Gradle and plugin info configured

---
+++ src/main/java/io/github/sree/LobManPlugin.java
@@ -0,0 +1,10 @@
+package io.github.sree;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LobManPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin started!");
+    }
+}
