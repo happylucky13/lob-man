c8535be7544c4d47c4421efb2a4f02d16deef05d
sree <happyluckypotato@gmail.com>
2026-07-21 20:12:08 -0400

    Wrote player detection logic in safetyNet

--- src/main/java/io/github/sree/safetynet/SafetyNet.java
+++ src/main/java/io/github/sree/safetynet/SafetyNet.java
@@ -1,6 +1,11 @@
 package io.github.sree.safetynet;
 
 import io.papermc.paper.command.brigadier.argument.position.ColumnBlockPosition;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class SafetyNet {
     private String name;
@@ -40,6 +45,37 @@ public class SafetyNet {
 
     public ColumnBlockPosition getSecondPosition() {
         return secondPosition;
+    }
+
+    // Detect if player is within bounds
+    @Nullable
+    public Player detectPlayer(World lobbyWorld) {
+        List<Player> lobbyPlayers = lobbyWorld.getPlayers();
+
+        int xOne = firstPosition.blockX();
+        int xTwo = secondPosition.blockX();
+        int zOne = firstPosition.blockZ();
+        int zTwo = secondPosition.blockZ();
+
+        for(Player player : lobbyPlayers) {
+
+            // Check player position
+            if(!(player.getY() <= yLevel)) {
+                continue;
+            }
+
+            if(!(player.getX() >= Math.min(xOne, xTwo) && player.getX() <= Math.max(xOne, xTwo))) {
+                continue;
+            }
+
+            if(!(player.getZ() >= Math.min(zOne, zTwo) && player.getZ() <= Math.max(zOne, zTwo))) {
+                continue;
+            }
+
+            return player;
+        }
+
+        return null;
     }
 
 
