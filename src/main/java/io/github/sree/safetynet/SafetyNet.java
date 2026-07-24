dc82d531e0c47d1222e215e8e2806e17fcf64f29
sree <happyluckypotato@gmail.com>
2026-07-24 01:38:15 -0400

    Attempt and fail at serialization and deserialization

--- src/main/java/io/github/sree/safetynet/SafetyNet.java
+++ src/main/java/io/github/sree/safetynet/SafetyNet.java
@@ -3,17 +3,18 @@ package io.github.sree.safetynet;
 import io.github.sree.LobmanPlugin;
 import io.papermc.paper.command.brigadier.argument.position.ColumnBlockPosition;
 import io.papermc.paper.math.BlockPosition;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitScheduler;
-import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class SafetyNet {
+public class SafetyNet implements ConfigurationSerializable {
     private final String name;
     private final int yLevel;
     private final ColumnBlockPosition firstPosition;
@@ -91,9 +92,83 @@ public class SafetyNet {
     }
 
     // Create safety net
-    public static void createSafetyNet(String name, int yLevel, ColumnBlockPosition firstPosition, ColumnBlockPosition secondPosition, BlockPosition teleportPosition) {
+    public static void createSafetyNet(String name, int yLevel, ColumnBlockPosition firstPosition, ColumnBlockPosition secondPosition, BlockPosition teleportPosition, LobmanPlugin plugin) {
         SafetyNet safetyNet = new SafetyNet(name, yLevel, firstPosition, secondPosition, teleportPosition);
-        var plugin = LobmanPlugin.getInstance();
         plugin.getSafetyNets().add(safetyNet);
+    }
+
+    @NotNull
+    public Map<String, Object> serialize() {
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("name", this.name);
+        data.put("y-level", this.yLevel);
+
+        data.put("first-position-x", this.firstPosition.blockX());
+        data.put("first-position-z", this.firstPosition.blockZ());
+
+        data.put("second-position-x", this.secondPosition.blockX());
+        data.put("second-position-z", this.secondPosition.blockZ());
+
+        data.put("teleport-position-x", this.teleportPosition.blockX());
+        data.put("teleport-position-y", this.teleportPosition.blockY());
+        data.put("teleport-position-z", this.teleportPosition.blockZ());
+
+        return data;
+    }
+
+    public static SafetyNet deserialize(Map<String, Object> args) {
+
+        // Messy ahh code, fix later
+        ColumnBlockPosition firstPosition = new ColumnBlockPosition() {
+            @Override
+            public int blockX() {
+                return (int) args.get("first-position-x");
+            }
+
+            @Override
+            public int blockZ() {
+                return (int) args.get("first-position-z");
+            }
+        };
+
+        ColumnBlockPosition secondPosition = new ColumnBlockPosition() {
+            @Override
+            public int blockX() {
+                return (int) args.get("second-position-x");
+            }
+
+            @Override
+            public int blockZ() {
+                return (int) args.get("second-position-z");
+            }
+        };
+
+        BlockPosition teleportPosition = new BlockPosition() {
+            @Override
+            public int blockX() {
+                return (int) args.get("teleport-position-x");
+            }
+
+            @Override
+            public int blockY() {
+                return (int) args.get("teleport-position-y");
+            }
+
+            @Override
+            public int blockZ() {
+                return (int) args.get("teleport-position-z");
+            }
+        };
+
+
+        return new SafetyNet(
+                (String) args.get("name"),
+                (int) args.get("y-level"),
+                firstPosition,
+                secondPosition,
+                teleportPosition
+        );
+
     }
 }
